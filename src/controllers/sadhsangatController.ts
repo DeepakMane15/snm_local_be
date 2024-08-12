5f2b6f53c8260c6eabdbe37474bfc2a376a4f169
DeepakFromZeus <manedeep2001@gmail.com>
2024-08-13 00:37:47 +0530

    add logger in sadhsangat apis

--- src/controllers/sadhsangatController.ts
+++ src/controllers/sadhsangatController.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from 'express';
 import sadhsangatService from '../services/sadhsangatService';
-import { SadhsangatDataModel } from '../models/sadhsangatDataModel';
+import logger from '../utils/winston';
 
 const createSadhsangat = async (req: Request, res: Response) => {
     try {
+        logger.info(`Create Sadhsangat record IN`);
         const sadhsangatData = req.body;
         const result = await sadhsangatService.createSadhsangat(sadhsangatData);
+        logger.info(`Create Sadhsangat record OUT`);
         res.status(201).json({ message: 'Record inserted successfully', id: result[0] });
     } catch (error) {
-        console.error(error);
+        logger.error(`Error creating Sadhsangat record: - ${error}`);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
 
 const fetchSadhsangat = async (req: Request, res: Response) => {
     try {
+        logger.info(`Fetch Sadhsangat records IN`);
         const id = parseInt(req.params.id, 10);
         const result: any = await sadhsangatService.getSadhsangat(id);
+        logger.info(`Fetch Sadhsangat records Out`);
         res.status(201).json({ message: 'Record fetched successfully', data: (result && result.length) ? result : null });
     } catch (error) {
-        console.error(error);
+        logger.error(`Error fetching Sadhsangat record: - ${error}`);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
@@ -27,10 +31,12 @@ const fetchSadhsangat = async (req: Request, res: Response) => {
 const fetchSadhsangatById = async (req: Request, res: Response) => {
     try {
         const id = parseInt(req.params.id, 10);
+        logger.info(`Fetch Sadhsangat by id record IN for id: ${id}`);
         const result: any = await sadhsangatService.getSadhsangatById(id);
+        logger.info(`Fetch Sadhsangat by id record OUT for id: ${id}`);
         res.status(201).json({ message: 'Record fetched successfully', data: result});
     } catch (error) {
-        console.error(error);
+        logger.error(`Error fetching Sadhsangat record by id: - ${error}`);
         res.status(500).json({ message: 'Internal Server Error' });
     }
 };
@@ -39,16 +45,20 @@ const fetchSadhsangatById = async (req: Request, res: Response) => {
 const updateSadhsangat = async (req: Request, res: Response) => {
     try {
         const id = parseInt(req.params.id, 10);
+        logger.info(`Updating Sadhsangat record IN for id: ${id}`);
         const updateData = req.body;
         
         const updated = await sadhsangatService.updateSadhsangat(id, updateData);
 
         if (updated) {
+            logger.info(`Updated Sadhsangat record for id: ${id}`);
             return res.status(200).json({ message: 'Sadhsangat record updated successfully' });
         } else {
+            logger.warn(`Sandhsangat record does not exist for id: ${id}`);
             return res.status(404).json({ message: 'Sadhsangat record not found' });
         }
     } catch (error) {
+        logger.error(`Error updating Sadhsangat record: - ${error}`);
         return res.status(500).json({ message: 'Error updating Sadhsangat record', error });
     }
 };
@@ -56,15 +66,19 @@ const updateSadhsangat = async (req: Request, res: Response) => {
 
 const deleteSadhsangat = async (req: Request, res: Response) => {
     const { id } = req.params;
+    logger.info(`Deleting Sadhsangat record IN for id: ${id}`);
 
     try {
         const result = await sadhsangatService.deleteSadhsangat(Number(id));
         if (result) {
+            logger.info(`Deleted Sadhsangat record for id: ${id}`);
             return res.status(200).json({ message: 'Sadhsangat record deleted successfully.' });
         } else {
+            logger.warn(`Sandhsangat record does not exist for id: ${id}`);
             return res.status(404).json({ message: 'Sadhsangat record not found.' });
         }
     } catch (error) {
+        logger.error(`Error deleting Sadhsangat record: - ${error}`);
         return res.status(500).json({ message: 'Error deleting Sadhsangat record.', error });
     }
 };
